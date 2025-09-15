4f25e01bcbce7bd4cbed729e36d9e54ec44b4bb4
farzane <farzanekazemi8517@gmail.com>
2025-09-15 17:35:37 +0330

    version2/ lose win scenario

--- script.js
+++ script.js
@@ -27,48 +27,76 @@ function playRound(computerChoice, humanChoice) {
     console.log("Both picked same choice, please try again!");
   }
 
-  if (humanChoice === "rock") {
-    if (computerChoice === "scissors") {
-      humanScore++;
-      console.log("You won!");
-    } else if (computerChoice === "paper") {
-      computerScore++;
-      console.log("You, lose!");
-    }
-  } else if (humanChoice === "paper") {
-    if (computerChoice === "scissors") {
-      computerScore++;
-      console.log("You, lose!");
-    } else if (computerChoice === "rock") {
-      humanScore++;
-      console.log("You won!");
-    }
-  } else if (humanChoice === "scissors") {
-    if (computerChoice === "paper") {
-      humanScore++;
-      console.log("You won!");
-    } else if (computerChoice === "rock") {
-      computerScore++;
-      console.log("You, lose!");
-    }
+  // Humanlose scenario
+  if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    computerScore++;
+    console.log(
+      `You, lose! ${computerChoice} beat ${humanChoice} | your score=${humanScore} / system score=${computerScore}`
+    );
   }
+
+  // Humanwin scenario
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log(
+      `You won! ${humanChoice} beat ${computerChoice} | your score=${humanScore} / system score=${computerScore}`
+    );
+  }
+
+  // if (humanChoice === "rock") {
+  //   if (computerChoice === "scissors") {
+  //     humanScore++;
+  //     console.log("You won!");
+  //   } else if (computerChoice === "paper") {
+  //     computerScore++;
+  //     console.log("You, lose!");
+  //   }
+  // } else if (humanChoice === "paper") {
+  //   if (computerChoice === "scissors") {
+  //     computerScore++;
+  //     console.log("You, lose!");
+  //   } else if (computerChoice === "rock") {
+  //     humanScore++;
+  //     console.log("You won!");
+  //   }
+  // } else if (humanChoice === "scissors") {
+  //   if (computerChoice === "paper") {
+  //     humanScore++;
+  //     console.log("You won!");
+  //   } else if (computerChoice === "rock") {
+  //     computerScore++;
+  //     console.log("You, lose!");
+  //   }
+  // }
 
   console.log("humanScore:", humanScore);
   console.log("computerScore: ", computerScore);
 }
 
-function playGame(rounds= 1) {
+function playGame(rounds = 1) {
   for (let i = 1; i <= rounds; i++) {
     playRound(getComputerChoice(), getHumanChoice());
   }
 
-  if (humanScore > computerScore) {
-    alert(`cangrats, you won! your score=${humanScore} | system score=${computerScore}`);
-  } else if (humanScore < computerScore) {
-    alert(`System won!system score=${computerScore} | your score=${humanScore}`);
+  if (humanScore === 5) {
+    alert(
+      `cangrats, you won! your score=${humanScore} | system score=${computerScore}`
+    );
+  } else if (computerScore === 5) {
+    alert(
+      `System won!system score=${computerScore} | your score=${humanScore}`
+    );
   } else {
     alert(`Both won! score=${humanScore}`);
   }
 }
 
-playGame(4);
+playGame(5);
